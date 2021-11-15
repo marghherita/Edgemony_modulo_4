be372dae027c936d3d8a2b2fc2e7711f224cc501
Margherita Grasso <margherita.grasso3@gmail.com>
2021-11-15 20:20:00 +0100

    Modifiche_1

--- 15-11-21/Esercizio/js/list.js
+++ 15-11-21/Esercizio/js/list.js
@@ -1,14 +1,33 @@
-import { render } from "./utils.js";
+// import { render } from "./utils.js";
+
+// const List = (data) => {
+//     const elements = data
+//         .map((item) => `<li ${item.title} - ${item.completed} - ${item.expires} </li>`)
+//         .join('');
+
+//     const container = document.querySelector('#container');
+//     render(container, `
+//         <p>To Do List</p>
+//         <ul> ${elements} </ul>`)
+// };
+
+// export { List };
+
+import { render } from "./utils.js"
 
 const List = (data) => {
     const elements = data
-        .map((item) => `<li ${item.title} - ${item.completed} - ${item.expires} </li>`)
+        .map(item => `
+            <li>
+            ${item.title} </br>
+            <input type="checkbox" ${item.completed ? "checked" : ""}>
+            - ${item.expires}
+            </li>`)
         .join('');
-
     const container = document.querySelector('#container');
     render(container, `
-        <p>To Do List</p>
-        <ul> ${elements} </ul>`);
+            <p>To Do List</p>
+            <ul> ${elements} </ul>`)
 };
 
-export { List };
+export { List }
